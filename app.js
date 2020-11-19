150036f84dd2eb36c9cba0df30b291701d6f5c85
pnguye11 <pnguye11@gmail.com>
2020-11-19 13:51:34 -0800

    simple api fetch

--- app.js
+++ app.js
@@ -1,120 +1,120 @@
-// /// sync code example
+/// sync code example
 
-// function otherFunction() {
-//   console.log("we are a another function");
-//   console.log("do something");
-// }
+function otherFunction() {
+  console.log("we are a another function");
+  console.log("do something");
+}
 
-// console.log("start");
+console.log("start");
 
-// otherFunction();
-// console.log("end");
+otherFunction();
+console.log("end");
 
-// /// async code example
+/// async code example
 
-// console.log("start");
+console.log("start");
 
-// setTimeout(() => {
-//   console.log("we are in a timeout");
-// }, 2000);
+setTimeout(() => {
+  console.log("we are in a timeout");
+}, 2000);
 
-// const items = [1, 2, 3, 4, 5];
+const items = [1, 2, 3, 4, 5];
 
-// items.forEach((item) => {
-//   console.log(item);
-// });
+items.forEach((item) => {
+  console.log(item);
+});
 
-// console.log("end");
+console.log("end");
 
-// ////callbacks and callback hell
+////callbacks and callback hell
 
-// console.log("start");
+console.log("start");
 
-// function loginUser(email, password, callback) {
-//   setTimeout(() => {
-//     console.log("now we have data");
-//     callback({ userEmail: email });
-//   }, 5000);
-// }
+function loginUser(email, password, callback) {
+  setTimeout(() => {
+    console.log("now we have data");
+    callback({ userEmail: email });
+  }, 5000);
+}
 
-// function getUserVideos(email, callback) {
-//   setTimeout(() => {
-//     callback(["video1", "video2"]);
-//   }, 1000);
-// }
-// function videoDetails(video, callback) {
-//   setTimeout(() => {
-//     callback("title of the video");
-//   });
-// }
-// const user = loginUser("hi@hi.com", 12345, (user) => {
-//   console.log(user);
-//   getUserVideos(user.userEmail, (videos) => {
-//     console.log(videos);
-//     videoDetails(videos[0], (title) => {
-//       console.log(title);
-//     });
-//   });
-// });
+function getUserVideos(email, callback) {
+  setTimeout(() => {
+    callback(["video1", "video2"]);
+  }, 1000);
+}
+function videoDetails(video, callback) {
+  setTimeout(() => {
+    callback("title of the video");
+  });
+}
+const user = loginUser("hi@hi.com", 12345, (user) => {
+  console.log(user);
+  getUserVideos(user.userEmail, (videos) => {
+    console.log(videos);
+    videoDetails(videos[0], (title) => {
+      console.log(title);
+    });
+  });
+});
 
-// console.log(user);
-// console.log("end");
+console.log(user);
+console.log("end");
 
-// ////promises
-
-// // const promise = new Promise((resolve, reject) => {
-// //   setTimeout(() => {
-// //     console.log("got the user");
-// //     resolve({ user: "ed" });
-// //   }, 2000);
-// // });
+////promises
 
 // const promise = new Promise((resolve, reject) => {
 //   setTimeout(() => {
 //     console.log("got the user");
-//     reject(new Error("user not loggin in"));
+//     resolve({ user: "ed" });
 //   }, 2000);
 // });
 
-// promise
-//   .then((user) => {
-//     console.log(user);
-//   })
-//   .catch((err) => console.log(err));
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("got the user");
+    reject(new Error("user not loggin in"));
+  }, 2000);
+});
 
-// console.log("start");
+promise
+  .then((user) => {
+    console.log(user);
+  })
+  .catch((err) => console.log(err));
 
-// function loginUser(email, password, callback) {
-//   setTimeout(() => {
-//     console.log("now we have data");
-//     callback({ userEmail: email });
-//   }, 5000);
-// }
+console.log("start");
 
-// function getUserVideos(email, callback) {
-//   setTimeout(() => {
-//     callback(["video1", "video2"]);
-//   }, 1000);
-// }
-// function videoDetails(video, callback) {
-//   setTimeout(() => {
-//     callback("title of the video");
-//   });
-// }
-// const user = loginUser("hi@hi.com", 12345, (user) => {
-//   console.log(user);
-//   getUserVideos(user.userEmail, (videos) => {
-//     console.log(videos);
-//     videoDetails(videos[0], (title) => {
-//       console.log(title);
-//     });
-//   });
-// });
+function loginUser(email, password, callback) {
+  setTimeout(() => {
+    console.log("now we have data");
+    callback({ userEmail: email });
+  }, 5000);
+}
 
-// console.log(user);
-// console.log("end");
+function getUserVideos(email, callback) {
+  setTimeout(() => {
+    callback(["video1", "video2"]);
+  }, 1000);
+}
+function videoDetails(video, callback) {
+  setTimeout(() => {
+    callback("title of the video");
+  });
+}
+const user = loginUser("hi@hi.com", 12345, (user) => {
+  console.log(user);
+  getUserVideos(user.userEmail, (videos) => {
+    console.log(videos);
+    videoDetails(videos[0], (title) => {
+      console.log(title);
+    });
+  });
+});
 
-///promise refactor
+console.log(user);
+console.log("end");
+
+/promise refactor
 
 console.log("start");
 
@@ -147,16 +147,59 @@ loginUser("ed", "lalala")
   .then((videos) => videoDetails(videos[0]))
   .then((detail) => console.log(detail));
 
-///sync
+// ///sync
 
-// const user = loginUser("hi@hi.com", 12345, (user) => {
-//   console.log(user);
-//   getUserVideos(user.userEmail, (videos) => {
-//     console.log(videos);
-//     videoDetails(videos[0], (title) => {
-//       console.log(title);
-//     });
-//   });
-// });
-///
+// // const user = loginUser("hi@hi.com", 12345, (user) => {
+// //   console.log(user);
+// //   getUserVideos(user.userEmail, (videos) => {
+// //     console.log(videos);
+// //     videoDetails(videos[0], (title) => {
+// //       console.log(title);
+// //     });
+// //   });
+// // });
+// ///
+// console.log("end");
+
+///async wait
+console.log("start");
+
+function loginUser(email, password) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("now we have data");
+      resolve({ userEmail: email });
+    }, 5000);
+  });
+}
+
+function getUserVideos(email) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(["video1", "video2"]);
+    }, 1000);
+  });
+}
+function videoDetails(video) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("title of the video");
+    });
+  });
+}
+
+// loginUser("ed", "lalala")
+//   .then((user) => getUserVideos(user.email))
+//   .then((videos) => videoDetails(videos[0]))
+//   .then((detail) => console.log(detail));
+
+///sync
+async function displayUser() {
+  const loggedUser = await loginUser("nate", 1234);
+  const videos = await getUserVideos(loggedUser.userEmail);
+  const detail = await videoDetails(videos[0]);
+  console.log(detail);
+}
+
+displayUser();
 console.log("end");
---
+++ app2.js
@@ -0,0 +1,16 @@
+//////simple api
+
+// / fetch("https://api.adviceslip.com/advice")
+//   .then((result) => result.json())
+//   .then((data) => console.log(data));
+
+const button = document.querySelector("button");
+const header = document.querySelector("h1");
+
+button.addEventListener("click", () => {
+  fetch("https://api.adviceslip.com/advice")
+    .then((result) => result.json())
+    .then((data) => {
+      header.innerText = data.slip.advice;
+    });
+});
